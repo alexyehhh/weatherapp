2b988b2f3644250c422dc7b36e83ebbe45f1eb3d
“Alex <alpyeh@ucsc.edu>
2023-05-01 18:06:13 -0700

    Add event listener to button

--- script.js
+++ script.js
@@ -20,3 +20,11 @@ function success(position){
 	// Print out the latitude and longitude to see if it works!
     console.log(latitude, longitude);
 }
+const btn = document.getElementById('getWeatherBtn');
+btn.addEventListener("click", () => {
+  let forecast = [["M", 52], ["Tu", 53], ["W", 54], ["Th", 55], ["F", 56]] //Nested array of predefined day/forecast pairs
+  let forecastElements = document.getElementsByClassName("forecast"); // Setting forecastElements to an array of divs with the class 'forecast' (5 in this case)
+  for (let i = 0; i < forecast.length; i++) { //For loop that goes from the 0th index to the length - 1 index
+      forecastElements[i].innerHTML = forecast[i][0] + ": " + forecast[i][1] + "\u00B0F"; //
+    }
+});
