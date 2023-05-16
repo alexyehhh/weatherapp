5fc8c63b45767f31fc3fe139e4d87b18a1331346
“Alex <alpyeh@ucsc.edu>
2023-05-15 17:51:08 -0700

    Delete hard-code

--- script.js
+++ script.js
@@ -49,11 +49,4 @@ btn.addEventListener("click", () => {
       forecastElements[i].innerHTML = `${forecast[i].dayName}: ${forecast[i].temp}°F`;
     }
   }
-
-
-  let forecast = [["M", 52], ["Tu", 53], ["W", 54], ["Th", 55], ["F", 56]] //Nested array of predefined day/forecast pairs
-  let forecastElements = document.getElementsByClassName("forecast"); // Setting forecastElements to an array of divs with the class 'forecast' (5 in this case)
-  for (let i = 0; i < forecast.length; i++) { //For loop that goes from the 0th index to the length - 1 index
-      forecastElements[i].innerHTML = forecast[i][0] + ": " + forecast[i][1] + "\u00B0F"; //
-    }
 });
